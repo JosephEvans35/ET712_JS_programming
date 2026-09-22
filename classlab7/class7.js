8c38f74b5a70d29c1d5b4e84379b5190030eba21
Joseph Evans <Joseph.Evans90@proton.me>
2026-09-22 15:52:29 -0400

    sting, conditional

---
+++ classlab7/class7.js
@@ -0,0 +1,24 @@
+console.log("Joseph Evans");
+console.log("Example 7: Array");
+let mixedarray = ['car', 10, true, 'apple', -3.5]
+console.log('The 4th value is ${mixedarray[3]}')
+console.log('origin array = ${mixedarray}')
+console.log("example 2: array methods")
+// remove the first value (left-most) in array
+
+
+
+
+
+
+
+
+
+console.log('Array after push method = ${mixedrry}')
+
+//METHOD THAT RETURN VALUE
+//find the index number of 'cherries'
+indexcherries = mixedarray.indexOf("cherries")
+console.log('what is the index for cherries ${indexcherries}')
+// it return  
+
